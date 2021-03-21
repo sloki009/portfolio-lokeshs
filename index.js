086a8ca747bd268d0c4920bcd6e7eb201c55684b
S <lokesh-kumar.s@capgemini.com>
2021-03-21 22:42:17 +0530

    added navlink active on scroll

--- index.js
+++ index.js
@@ -2,32 +2,43 @@ window.addEventListener("DOMContentLoaded", function () {
   setTimeout(() => {
     document.getElementById("page_loader_wrapper").classList.add("loaded");
   }, 500);
+  let mainNavLinks = document.querySelectorAll(
+    "#navbar_links a:not(#resume_link)"
+  );
+  let mainSections = document.querySelectorAll("header section");
 
   window.addEventListener("scroll", () => {
     let styleObj = {};
 
-    console.log("scrollY", window.scrollY);
-
     if (window.scrollY > 64) {
-      //navElem.style.background = "#000";
       styleObj["navbgColor"] = "#000";
       styleObj["aColor"] = "rgba(255,255,255,.75)";
     } else if (window.scrollY < 64) {
-      //navElem.style.background = "none";
       styleObj["navbgColor"] = "none";
       styleObj["aColor"] = "rgba(255,255,255,.75)";
     }
     if (window.scrollY > 440) {
-      //navElem.style.background = "#fff";
       styleObj["navbgColor"] = "#fff";
       styleObj["aColor"] = "rgba(15, 15, 15, 0.75)";
     }
-    console.log(styleObj);
     handleNavbar(styleObj);
+
+    let fromTop = window.scrollY - 50;
+    mainNavLinks.forEach((link) => {
+      let section = document.querySelector(link.hash);
+      if (
+        section.offsetTop <= fromTop + 55 &&
+        section.offsetTop + section.offsetHeight > fromTop + 55
+      ) {
+        link.classList.add("nav_current");
+      } else {
+        link.classList.remove("nav_current");
+      }
+    });
   });
 
   function handleNavbar({ navbgColor, aColor }) {
-    console.log("inhandlenavbarr...", navbgColor, aColor);
+    //console.log("inhandlenavbarr...", navbgColor, aColor);
     let navElem = document.querySelector("#nav_wrapper");
     navElem.style.background = navbgColor;
     let navlinks = document.querySelectorAll("#navbar_links a");
